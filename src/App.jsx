9ae807c8dcd1adcf0e0dde2a6745afa30f7fe96c
mo7amedehab97 <gazacityface@gmail.com>
2023-02-15 12:28:51 +0200

    create expierence and works section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Advantages from "./components/Advantages/Advantages";
 import Banner from "./components/Banner/Banner";
 import SeconedBanner from "./components/Banner/SeconedBanner";
+import Expereince from "./components/Experience/Expereince";
 import NavBar from "./components/NavBar/NavBar";
 import ShowCases from "./components/ShowCases/ShowCases";
+import Title from "./components/Title/Title";
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
       <Banner />
       <SeconedBanner />
       <ShowCases />
+      <Title title="المهارات" />
+      <Advantages />
+      <Title title="الخبرة والأعمال " />
+      <Expereince />
     </>
   );
 }
---
+++ src/components/Advantages/Advantages.jsx
@@ -0,0 +1,23 @@
+import "./index.css";
+import ico1 from "../../assets/Logo.png";
+const Advantages = () => {
+  return (
+    <section className="advantages_container">
+      <div className="advantage_holder">
+        <img src={ico1} alt="" />
+        <h2>تحسين محركت البحث</h2>
+      </div> <div className="advantage_holder">
+        <img src={ico1} alt="" />
+        <h2>تحسين محركت البحث</h2>
+      </div> <div className="advantage_holder">
+        <img src={ico1} alt="" />
+        <h2>تحسين محركت البحث</h2>
+      </div> <div className="advantage_holder">
+        <img src={ico1} alt="" />
+        <h2>تحسين محركت البحث</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Advantages;
