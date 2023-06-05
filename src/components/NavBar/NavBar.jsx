7b8fd3e23bba58ce9a459f2f30a4baad605356f0
mo7amed-helles <mohammed.helles@nadsoft.net>
2023-06-05 17:43:37 +0300

    new changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,13 @@
 import React, { useState,useRef } from "react";
-import emailjs from 'emailjs-com';
 
 import "./index.css";
 import Logo from "../../assets/مفرغ للموقع-04.png";
 import burgerImg from "../../assets/burger.svg";
 import { useNavigate } from "react-router-dom";
-import close from "../../assets/cancel.svg"
+import { InlineWidget,useCalendlyEventListener } from "react-calendly";
+
 const NavBar = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const [drop, setDrop] = useState(false);
   
   const navigate = useNavigate();
 
@@ -18,33 +17,14 @@ const NavBar = () => {
   setShowMenu(!showMenu);
   }
   const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault(); // Prevents the default form submission behavior
-
-    const form = event.target;
-    const nameInput = form.elements.user_name;
-    const emailInput = form.elements.user_email;
-    const messageInput = form.elements.message;
-
-    if (
-      nameInput.value.trim() === '' ||
-      emailInput.value.trim() === '' ||
-      messageInput.value.trim() === ''
-    ) {
-      alert('رجاءا قم بتعبئة الخانات المطلوبة'); // Display an error message
-      return; // Stop the function execution
-    }
-    emailjs.sendForm('service_6q5xyhm', 'template_acdesw6', form.current, 'XcGsom-zzVzAk9uGO')
-      .then((result) => {
-          // show the user a success message
-          console.log(result, "success")
-          setToggle(false)
-          setErr("")
-      }, (error) => {
-console.log(error,'fuckhere')
-      setErr("حدث خطأ ما يرجي المحاولة مرة اخري")
-      });
-  };
+  useCalendlyEventListener({
+    onProfilePageViewed: () => console.log("onProfilePageViewed"),
+    onDateAndTimeSelected: () => console.log("onDateAndTimeSelected"),
+    onEventTypeViewed: () => console.log("onEventTypeViewed"),
+    onEventScheduled: (e) => {
+      setToggle(false)
+    },
+  });
   console.log(toggle)
   return (
     <nav className="nav_container">
@@ -89,40 +69,13 @@ console.log(error,'fuckhere')
           justifyContent: "center", 
           alignItems: "center"
         }}>
- <form ref={form} onSubmit={sendEmail} className="form_send">
-  <div className="form_header">
-    <button type="button" onClick={()=>{
-      setToggle(false)
-    }}>
-    <img src={close} alt="close form " />
-    </button>
-  </div>
-  <div className="form_row">
-  <label>الاسم</label>
-     <input type="text" name="user_name" />
-  </div>
-    <div className="form_row">
+          <div style={{
+          width: "100%",
+          }}>
+            
+      <InlineWidget url="https://calendly.com/mo7amedehab" />
+          </div>
 
-     <label>الايميل</label>
-     <input type="email" name="user_email" />
-    </div>
-    <div className="form_row">
-
-     <label>رسالة</label>
-     <textarea name="message" />
-    </div>
-
-    {
-      err && <span style={{
-        color: "red"
-      }}>
-        {err}
-      </span>
-    }
-    <div className="form_send_container">
-     <input type="submit" value="ارسل" />
-    </div>
-   </form>
         </div>
       }
     </nav>
